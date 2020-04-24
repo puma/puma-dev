af8cd332a237673049b3b997b2cf20a623bd5049
Alan Norton <git@nonration.al>
2020-04-24 11:22:53 -0400

    finish setup and teardown for setup test

--- dev/setup_test.go
+++ dev/setup_test.go
@@ -1,41 +1,56 @@
 package dev
 
 import (
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strconv"
 	"testing"
 
-	. "github.com/puma/puma-dev/dev/devtest"
+	// . "github.com/puma/puma-dev/dev/devtest"
 
-	"github.com/puma/puma-dev/homedir"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestInstallIntoSystem(t *testing.T) {
-	appLinkDir := homedir.MustExpand("~/.gotest-dev-puma-dev")
-	tmpDir := homedir.MustExpand("~/.gotest-dev-puma-dev-homedir")
+	appLinkDir, _ := ioutil.TempDir("", ".puma-dev")
+	libDir, _ := ioutil.TempDir("", "Library")
+	logFilePath := filepath.Join(libDir, "Logs", "puma-dev.log")
+	launchAgentDir := filepath.Join(libDir, "LaunchAgents")
+	expectedPlistPath := filepath.Join(launchAgentDir, "io.puma.dev.plist")
+	assert.NoDirExists(t, launchAgentDir)
 
-	defer MakeDirectoryOrFail(t, appLinkDir)()
-	defer MakeDirectoryOrFail(t, tmpDir)()
-
-	launchAgentDir := filepath.Join(tmpDir, "Library", "LaunchAgents")
-	assert.False(t, DirExists(launchAgentDir))
+	defer func() {
+		exec.Command("launchctl", "unload", expectedPlistPath).Run()
+		os.RemoveAll(appLinkDir)
+		os.RemoveAll(logFilePath)
+		os.RemoveAll(libDir)
+	}()
 
 	err := InstallIntoSystem(&InstallIntoSystemArgs{
 		ListenPort:         10080,
 		TlsPort:            10443,
 		Domains:            "test:localhost",
-		Timeout:            "10s",
+		Timeout:            "5s",
 		ApplinkDirPath:     appLinkDir,
 		LaunchAgentDirPath: launchAgentDir,
-		LogfilePath:        "~/.gotest-dev-puma-dev-homedir/Library/Logs/puma-dev.log",
+		LogfilePath:        logFilePath,
 	})
 
 	assert.NoError(t, err)
 
-	info, err := os.Stat(launchAgentDir)
-	if assert.NoError(t, err) {
-		assert.Equal(t, 0755, info.Mode())
+	assert.DirExists(t, launchAgentDir)
+	assert.FileExists(t, expectedPlistPath)
+	AssertDirUmask(t, "0755", launchAgentDir)
+}
+
+func AssertDirUmask(t *testing.T, expectedUmask, path string) {
+	info, err := os.Stat(path)
+	if !assert.NoError(t, err) {
 		assert.True(t, info.IsDir())
+
+		actualUmask := "0" + strconv.FormatInt(int64(info.Mode().Perm()), 8)
+		assert.Equal(t, expectedUmask, actualUmask)
 	}
 }
