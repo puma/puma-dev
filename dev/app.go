puma/puma-dev

commit 566b636a6ffa8785bc025a5c8cfe14fe49f641d6

Prevent apps with the same folder name from colliding (#270)
before this commit if two symlinks were targeting distinct folders having the same name the app were considered to be the same

dev/app.go
@@ -3,6 +3,7 @@ package dev
 import (
 	"bufio"
 	"bytes"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -514,8 +515,10 @@ func (a *AppPool) App(name string) (*App, error) {
 	destStat, err := os.Stat(destPath)
 	if err == nil {
 		destName := destStat.Name()
-		if destName != canonicalName {
-			canonicalName = destName
+		if destName != name {
+			h := sha1.New()
+			h.Write([]byte(destPath))
+			canonicalName = fmt.Sprintf("%s-%.4x", destStat.Name(), h.Sum(nil))
 			aliasName = name
 		}
 	}

cmd/puma-dev/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -237,8 +238,12 @@ func runPlatformAgnosticTestScenarios(t *testing.T) {
 			panic(err)
 		}
 
-		assert.NoError(t, pollForEvent(t, "rack-hi-puma", "killing_app", "restart.txt touched"))
-		assert.NoError(t, pollForEvent(t, "rack-hi-puma", "shutdown", ""))
+		appRoot := filepath.Join(ProjectRoot, "etc", "rack-hi-puma")
+		h := sha1.New()
+		h.Write([]byte(appRoot))
+		appName := fmt.Sprintf("rack-hi-puma-%.4x", h.Sum(nil))
+		assert.NoError(t, pollForEvent(t, appName, "killing_app", "restart.txt touched"))
+		assert.NoError(t, pollForEvent(t, appName, "shutdown", ""))
 	})
 
 	t.Run("unknown app", func(t *testing.T) {

commit bda901036dbe0fee71712ad7da8340fe03c7919b

Fix: sharing app instances with different hostnames not working
This fixes a problem where it fails to reuse the same instance of the app
when using multiple hostnames with different names (multiple symlinks pointing
to the same folder).

The bug was introduced in #270. The patch is based on adding the checksum in
all cases when a symlink is involved, not only when the names don't match. If not,
it can happen that it's not added if the name matches for the first execution, which
results in not finding the app instance later, resulting in an error "There is already
a server bound to..."

Fixes #280

dev/app.go
@@ -521,10 +521,11 @@ func (a *AppPool) App(name string) (*App, error) {
 	destStat, err := os.Stat(destPath)
 	if err == nil {
 		destName := destStat.Name()
+		h := sha1.New()
+		h.Write([]byte(destPath))
+		canonicalName = fmt.Sprintf("%s-%.4x", destStat.Name(), h.Sum(nil))
+
 		if destName != name {
-			h := sha1.New()
-			h.Write([]byte(destPath))
-			canonicalName = fmt.Sprintf("%s-%.4x", destStat.Name(), h.Sum(nil))
 			aliasName = name
 		}
 	}