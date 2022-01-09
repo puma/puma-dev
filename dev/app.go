0530e3ae78bb4decd28339a063401fd4413c03c4
Alan Norton <git@nonration.al>
2022-01-09 11:27:16 -0500

    testing

--- dev/app.go
+++ dev/app.go
@@ -238,6 +238,9 @@ func (a *App) Log() string {
 }
 
 const executionShell = `exec bash -c '
+# enable bash job control
+set -m
+
 cd %s
 
 if test -e ~/.powconfig && [ "$PUMADEV_SOURCE_POWCONFIG" != "0" ]; then
